0eeb01f5b63b5ebf66296aaad008b360990918b1
Misha Shmidt <mishashmidt0@gmail.com>
2022-03-29 20:43:17 +0300

    feat: add server

--- server/core/passport.ts
+++ server/core/passport.ts
@@ -1,17 +1,21 @@
+import passport from 'passport';
 import GitHubStrategy from 'passport-github'
-import passport from 'passport'
+
 
 passport.use('github', new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    const fullName = profile.displayName || profile.username
-    const avatar = profile.photos[0].value
-    console.log('profile', profile);
-    cb()
-  }
-));
+        clientID: process.env.GITHUB_CLIENT_ID as string,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        callbackURL: "http://localhost:5000/auth/github/callback"
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb: any) {
+        console.log(accessToken, refreshToken, profile)
 
+        const user = {
+            fullname: profile.displayName,
+            avatarUrl: profile.photos?.[0].value,
+        }
+        cb();
+    }
+));
 export {passport}
+
