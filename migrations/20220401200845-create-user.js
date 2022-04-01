c488e470d7d3b9d5529322f467b0a7569c48ce38
Misha Shmidt <mishashmidt0@gmail.com>
2022-04-01 23:55:21 +0300

    feat: add connect db

---
+++ migrations/20220401200845-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            fullname: {
+                type: Sequelize.STRING
+            },
+            avataUrl: {
+                type: Sequelize.STRING
+            },
+            isActive: {
+                type: Sequelize.STRING
+            },
+            username: {
+                type: Sequelize.STRING
+            },
+            phone: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('users');
+    }
+};
